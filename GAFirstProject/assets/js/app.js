7e7d84c48f0960a780a9dc684c82b7faad69816d
Madhav Pokhrel <madhavpokhrel05@gmail.com>
2020-02-12 18:20:20 -0500

    Added more Jquery
    Added more JQuery and complete the project.

--- GAFirstProject/assets/js/app.js
+++ GAFirstProject/assets/js/app.js
@@ -31,5 +31,49 @@ $(document).ready(()=>{
       $(".email1").html(result.results[0].email);
     }});
    })
+// ************************************************************************
+   // button 2
+ $.ajax({url: "https://randomuser.me/api/", success: (result)=>{
+      // insert API value in html tag
+        $('.img-src2').attr('src',result.results[0].picture.large);
+      $(".name2").html(result.results[0].name.first)
+      $(".gender2").html(result.results[0].gender)
+      $(".city2").html(result.results[0].location.city)
+      $(".email2").html(result.results[0].email);
+    }});
+
+    $(".button2 > button").click(()=>{
+     $.ajax({url: "https://randomuser.me/api/", success:(result)=>{
+      // insert API value in html tag
+        $('.img-src2').attr('src',result.results[0].picture.large);
+      $(".name2").html(result.results[0].name.first)
+      $(".gender2").html(result.results[0].gender)
+      $(".city2").html(result.results[0].location.city)
+      $(".email2").html(result.results[0].email);
+    }});
+   })
+
+// *********  card three img name etc load form here ************
+
+// button 3
+ $.ajax({url: "https://randomuser.me/api/", success: (result)=>{
+      // insert API value in html tag
+        $('.img-src3').attr('src',result.results[0].picture.large);
+      $(".name3").html(result.results[0].name.first)
+      $(".gender3").html(result.results[0].gender)
+      $(".city3").html(result.results[0].location.city)
+      $(".email3").html(result.results[0].email);
+    }});
+
+   $(".button3 > button").click(()=>{
+     $.ajax({url: "https://randomuser.me/api/", success: (result)=>{
+      // insert API value in html tag
+        $('.img-src3').attr('src',result.results[0].picture.large);
+      $(".name3").html(result.results[0].name.first)
+      $(".gender3").html(result.results[0].gender)
+      $(".city3").html(result.results[0].location.city)
+      $(".email3").html(result.results[0].email);
+    }});
+   })
 
 });
